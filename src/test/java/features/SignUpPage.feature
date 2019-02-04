b6fbda5d73ca973b58f6a59c2fae7a4e8aa61290
Mohamed Hassan <mohamedhassan@Mohameds-MacBook-Pro.local>
2019-02-04 21:58:45 +0200

    Sign Up Scenarios

--- src/test/java/features/SignUpPage.feature
+++ src/test/java/features/SignUpPage.feature
@@ -1,10 +1,81 @@
 Feature: Sign Up
 
-  @Smoke
-  Scenario: Verify that the user can sign up
+  Background: Open Fly365 Site
     Given Navigate to URl
+
+  Scenario: Verify that the user can sign up
     And   Open Sign up page
+    Then  'Sign Up' page is opened
     And   Fill required data
     And   Click on Create Account
     Then  The user created successfully
     And   The new record set on database
+
+
+  Scenario: Verify that the user can't sign up without the mandatory fields
+    And   Open Sign up page
+    Then  'Sign Up' page is opened
+    And   Click on Create Account
+    Then  The system display validation messages for all mandatory fields
+
+
+  Scenario Outline: Verify that the user can't sign up with invalid first name
+    And  Open Sign up page
+    Then 'Sign Up' page is opened
+    And  Check validation when enter <invalidName> First Name
+    Examples:
+      |invalidName|
+      | 1234567 |
+      | !@#$%^& |
+      |حرف عربي |
+
+
+  Scenario Outline: Verify that the user can't sign up with invalid last name
+    And  Open Sign up page
+    Then 'Sign Up' page is opened
+    And  Check validation when enter <invalidName> Last Name
+    Examples:
+      |invalidName|
+      | 1234567 |
+      | !@#$%^& |
+      |حرف عربي |
+
+
+  Scenario Outline: Verify that the user can't sign up with invalid email
+    And  Open Sign up page
+    Then 'Sign Up' page is opened
+    And  Enter First Name
+    And  Enter Last Name
+    And  Check validation when enter <invalidEmails> Email Address
+    Examples:
+      | invalidEmails        |
+      | plainttext           |
+      | omda@@mailinator.com |
+      | omda @mailinator.com |
+      | omda@mail nantor.com |
+      | omda@mailinator      |
+      |om@mailinator.com     |
+
+
+  Scenario Outline: Verify that the user can't sign up with password less than 8 characters
+    And  Open Sign up page
+    Then 'Sign Up' page is opened
+    And  Enter First Name
+    And  Enter Last Name
+    And  Enter Email
+    And  Check validation when enter <invalidPass> Password
+    Examples:
+    |invalidPass|
+    | 123       |
+    | 1234567   |
+    | 123456789012345678901234567890123456789012345678901|
+
+
+#  Scenario: Verify that the user can't sign up with the same email twice
+#    Given Insert new user in database
+#    And Open Sign up page
+#    And 'Sign Up' page is opened
+#    And Enter First Name
+#    And Enter Last Name
+#    And Enter Email
+
--- src/test/java/step_definition/SignUpTest.java
+++ src/test/java/step_definition/SignUpTest.java
@@ -21,7 +21,20 @@ public class SignUpTest extends TestBase {
     By emailTxt = By.xpath("//input[@placeholder='john@example.com']");
     By passwordTxt = By.xpath("//input[@placeholder='******************']");
     By creatAccountBtn = By.xpath("//button[text()='CREATE ACCOUNT']");
+    By validationNameMessage = By.xpath("//span[text()='Name must be letters only']");
+    By validationEmailMessage = By.xpath("//span[text()='Please enter a valid email']");
+    By validationPasswordMessage = By.xpath("//span[text()='Password length must be between 8 to 50 characters']");
+    By firstNameRequiredMessage = By.xpath("//span[text()='Please enter first name']");
+    By lastNameRequiredMessage = By.xpath("//span[text()='Please enter last name']");
+    By emailRequiredMessage = By.xpath("//span[text()='Please enter email']");
+    By passwordRequiredMessage = By.xpath("//span[text()='Please enter password']");
 
+
+
+//    @Given("^Insert new user in database$")
+//    public void insertNewUserInDatabase() {
+//
+//    }
 
     @And("Open Sign up page")
     public void go_to_signup_page()  {
@@ -35,14 +48,73 @@ public class SignUpTest extends TestBase {
         //need specific data with integration with database
         driver.findElement(firstNameTxt).sendKeys("John");
         driver.findElement(lastNameTxt).sendKeys("Smith");
-        driver.findElement(emailTxt).sendKeys("mido@mailinator.com");
+        driver.findElement(emailTxt).sendKeys("omda@mailinator.com");
         driver.findElement(passwordTxt).sendKeys(fakerdata.internet().password());
+    }
+
+    @And("Enter First Name")
+    public void fill_firstName()
+    {
+        driver.findElement(firstNameTxt).sendKeys("John");
+    }
+
+    @And("Enter Last Name")
+    public void fill_lastName()
+    {
+        driver.findElement(lastNameTxt).sendKeys("Smith");
+    }
+
+    @And("Enter Email")
+    public void fill_email()
+    {
+        Faker fakerdata = new Faker();
+        driver.findElement(emailTxt).sendKeys(fakerdata.internet().emailAddress());
+    }
+
+    @And("^Check validation when enter (.*) First Name$")
+    public void fill_firstName(String fName)
+    {
+        driver.findElement(firstNameTxt).sendKeys(fName);
+        String error_message = driver.findElement(validationNameMessage).getText();
+        Assert.assertEquals(error_message,"Name must be letters only");
+    }
+
+
+    @And("^Check validation when enter (.*) Last Name$")
+    public void fill_lastName(String fName)
+    {
+        driver.findElement(lastNameTxt).sendKeys(fName);
+        String error_message = driver.findElement(validationNameMessage).getText();
+        Assert.assertEquals(error_message,"Name must be letters only");
+    }
+
+    @And("^Check validation when enter (.*) Email Address$")
+    public void fill_email(String email)
+    {
+        driver.findElement(emailTxt).sendKeys(email);
+        String error_message = driver.findElement(validationEmailMessage).getText();
+        Assert.assertEquals(error_message,"Please enter a valid email");
+    }
+
+    @And("^Check validation when enter (.*) Password$")
+    public void fill_invPassword(String invPass)
+    {
+        driver.findElement(passwordTxt).sendKeys(invPass);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(validationPasswordMessage));
+        String pass_error = driver.findElement(validationPasswordMessage).getText();
+        Assert.assertEquals(pass_error,"Password length must be between 8 to 50 characters");
+    }
+
+    @And("Enter Password")
+    public void fill_password()
+    {
+        driver.findElement(passwordTxt).sendKeys("12345678");
     }
 
     @And("Click on Create Account")
     public void Click_button()
     {
-
+        wait.until(ExpectedConditions.visibilityOfElementLocated(SignUpHeader));
         driver.findElement(creatAccountBtn).click();
     }
 
@@ -50,10 +122,10 @@ public class SignUpTest extends TestBase {
     @And("The new record set on database")
     public void Check_DataBase()
     {
-        DataBase.execute_query_dbs("k8stage1.cl9iojf4kdop.eu-west-1.rds.amazonaws.com:5432","user_api","select email from users where email='mido@mailinator.com'");
+        DataBase.execute_query_dbs("k8stage1.cl9iojf4kdop.eu-west-1.rds.amazonaws.com:5432","user_api","select email from users where email='omda@mailinator.com'");
         System.out.println(DataBase.data);
-        Assert.assertEquals(DataBase.data,"mido@mailinator.com");
-        DataBase.execute_query_dbs("k8stage1.cl9iojf4kdop.eu-west-1.rds.amazonaws.com:5432","user_api","delete from users where email='mido@mailinator.com'");
+        Assert.assertEquals(DataBase.data,"omda@mailinator.com");
+        DataBase.execute_query_dbs("k8stage1.cl9iojf4kdop.eu-west-1.rds.amazonaws.com:5432","user_api","delete from users where email='omda@mailinator.com'");
     }
 
     @Then("The user created successfully")
@@ -68,6 +140,15 @@ public class SignUpTest extends TestBase {
         wait.until(ExpectedConditions.visibilityOfElementLocated(SignUpHeader));
         String headerText = driver.findElement(SignUpHeader).getText();
         Assert.assertEquals(headerText,"Sign in or sign up");
-
     }
+
+    @Then("The system display validation messages for all mandatory fields")
+    public void leave_mandatory_fields()
+    {
+        Assert.assertEquals(firstNameRequiredMessage.toString(),"Please enter first name");
+        Assert.assertEquals(lastNameRequiredMessage.toString(),"Please enter last name");
+        Assert.assertEquals(emailRequiredMessage.toString(),"Please enter email");
+        Assert.assertEquals(passwordRequiredMessage.toString(),"Please enter password");
+    }
+
 }
