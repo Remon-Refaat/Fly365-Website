9b246fb734febffd8859bfa1e6c7c9eac7763cad
mahmoudsayed89 <mahmoud.sayed@fly365.com>
2019-02-04 22:52:21 +0200

    Adding Forget password

--- src/test/java/features/SignInPage.feature
+++ src/test/java/features/SignInPage.feature
@@ -1,14 +1,13 @@
 Feature: login to account
   login to a created account
 
-
   Scenario: verify that uer can't login with unregistered data
     Given Navigate to URl
     And open login page
     And user enter unregistered email
     And user enter the right password
     When the user click on login button
-    Then user shall see error message
+    Then user shall see InValid Login Error Message
 
   Scenario: login into account with wrong password
     Given Navigate to URl
@@ -16,7 +15,7 @@ Feature: login to account
     And user enter a valid email
     And user enter the wrong password
     When the user click on login button
-    Then user shall see error message
+    Then user shall see InValid Login Error Message
 
   Scenario: login into account with password less than 8 chars
     Given Navigate to URl
@@ -34,6 +33,21 @@ Feature: login to account
     When the user click on login button
     Then user shall see password error message
 
+  Scenario: login into account with empty password
+    Given Navigate to URl
+    And open login page
+    And user enter a valid email
+    And user enter empty password
+    When the user click on login button
+    Then user shall see empty password error message
+
+  Scenario: Login into account with empty email
+    Given Navigate to URl
+    And open login page
+    And user enter an empty email
+    And user enter the right password
+    When the user click on login button
+    Then user shall see email empty error message
 
   Scenario: Login into account with invalid email formation
     Given Navigate to URl
@@ -42,24 +56,6 @@ Feature: login to account
     And user enter the right password
     When the user click on login button
     Then user shall see email error message
-
-  Scenario: Login into account with empty email
-    Given Navigate to URl
-    And open login page
-    And user enter an empty email
-    And user enter the right password
-    When the user click on login button
-    Then user shall see email error message
-
-
-  Scenario: login into account with empty password
-    Given Navigate to URl
-    And open login page
-    And user enter a valid email
-    And user enter empty password
-    When the user click on login button
-    Then user shall see password error message
-
 
   Scenario: Login into account with correct details
     Given Navigate to URl
@@ -72,7 +68,6 @@ Feature: login to account
     And user logout
     And delete new user at database
 
-
   Scenario: Login into account with correct Upper case email
     Given Navigate to URl
     And insert new user at database
@@ -83,3 +78,36 @@ Feature: login to account
     Then the user shall be redirect to my booking page
     And user logout
     And delete new user at database
+
+  Scenario: Customer forget password with exist email
+    Given Navigate to URl
+    And insert new user at database
+    And open login page
+    And press on forget password link
+    And enter a valid email at forget password text
+    When click on send email button
+    Then page shall be redirect to login page
+    And delete new user at database
+
+  Scenario: Customer forget password with empty email
+    Given Navigate to URl
+    And open login page
+    And press on forget password link
+    When click on send email button
+    Then user shall see empty email error message at forget password page
+
+  Scenario: Customer forget password with invalid email
+    Given Navigate to URl
+    And open login page
+    And press on forget password link
+    And enter invalid email formation at email text at forget password page
+    When click on send email button
+    Then user shall see email error message at forget password page
+
+  Scenario: Customer forget password with Unregistered email
+    Given Navigate to URl
+    And open login page
+    And press on forget password link
+    And enter unregistered email at forget password page
+    When click on send email button
+    Then user shall see email not registered error message at forget password page
--- src/test/java/step_definition/SignInTest.java
+++ src/test/java/step_definition/SignInTest.java
@@ -1,5 +1,6 @@
 package step_definition;
 
+import com.github.javafaker.Faker;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -15,153 +16,163 @@ import org.testng.Assert;
 
 public class SignInTest extends TestBase {
 
-    WebDriverWait wait = new WebDriverWait(driver, 10);
+    private WebDriverWait wait = new WebDriverWait(driver, 60);
+    private Faker fakerLogin = new Faker();
 
-    //Faker fakerlogin = new Faker();
-    //String Email = fakerlogin.internet().emailAddress();
-    //String password = fakerlogin.internet().password();
-    String userEmail = "m.sayed.89@gmail.com";
-    String userHashPassWord = "$2y$04$E3GLR2vVV0AKfvwm6L0MDeKpVfFw4kR58wb9ohNN.TpGoF6fdpoK.\n";
-    String userPassWord = "@Test123";
-    String hostName = "k8stage1.cl9iojf4kdop.eu-west-1.rds.amazonaws.com:5432";
-    String dbsName = "user_api";
+    private String userEmail = "m.sayed.89@gmail.com";
+    private String userHashPassWord = "$2y$04$E3GLR2vVV0AKfvwm6L0MDeKpVfFw4kR58wb9ohNN.TpGoF6fdpoK.\n";
+    private String userPassWord = "@Test123";
+    private String hostName = "k8stage1.cl9iojf4kdop.eu-west-1.rds.amazonaws.com:5432";
+    private String dbsName = "user_api";
 
-
-    By LoginHeaderBtn = By.xpath("/html/body/div[1]/div[1]/div/div/div[2]/div/div[4]/a");
-    By LoginEmailTxt = By.xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/form/div[2]/div[1]/div/div/div[2]/div/div/div[1]/input");
-    By LoginPassWordTxt = By.xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/form/div[2]/div[2]/div/div/div[2]/div/div/div[1]/div/input");
-    By LoginBtn = By.xpath("/html/body/div[1]/div[2]/div/div[2]/div[3]/div/form/div[2]/div[4]/div/button");
-    By ProfileNameBtn = By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[4]/span[1]");
-    By LogoutBtn = By.xpath("//li[contains(text(),'Sign Out')]");
+    private By LoginBTN = By.xpath("//button[contains(text(),'Login')]");
+    private By LoginHeaderBTN = By.xpath("//a[@class='text-black md:w-24 text-center w-full block float-right hover:text-white bg-white btn hover:bg-primary-second font-normal py-2 px-5 rounded-sm text-sm no-underline']");
+    private By LoginEmailTXT = By.xpath("//input[@placeholder='john@example.com']");
+    private By LoginPassWordTXT = By.xpath("//input[@placeholder='******************']");
+    private By ProfileNameBTN = By.xpath("//span[@class='el-dropdown-link capitalize text-xs text-white el-dropdown-selfdefine']");
+    private By LogoutBTN = By.xpath("//li[contains(text(),'Sign Out')]");
+    private By AccountSettingBTN = By.xpath("//a[@class='account-links__link text-sm flex font-medium items-center link link-with-icon']");
+    private By InvalidLogInMSG = By.xpath("//div[@class='tooltip error error']//span[@class='tooltiptext']");
+    private By PassWordErrorMSG = By.xpath("//span[@class='tooltiptext with-arrow']");
+    private By EmailErrorMSG = By.xpath("//span[@class='tooltiptext with-arrow']");
+    private By HomePageTitle = By.xpath("//span[@class='text-primary-second']");
 
 
     @And("^open login page$")
     public void openLoginPage() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginHeaderBtn));
-        driver.findElement(LoginHeaderBtn).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginHeaderBTN));
+        driver.findElement(LoginHeaderBTN).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginBTN));
+        WebElement LoginPassWordTXT = driver.findElement(LoginBTN);
+        Assert.assertTrue(LoginPassWordTXT.isDisplayed());
     }
 
     @And("^user enter a valid email$")
     public void userEnterAValidEmail() {
-        driver.findElement(LoginEmailTxt).sendKeys(userEmail);
-        //driver.findElement(LoginEmail).sendKeys(fakerlogin.internet().emailAddress());
-
+        driver.findElement(LoginEmailTXT).sendKeys(userEmail);
     }
 
     @And("^user enter the right password$")
     public void userEnterTheRightPassword() {
-
-        driver.findElement(LoginPassWordTxt).sendKeys(userPassWord);
-        //driver.findElement(LoginPassWord).sendKeys(fakerlogin.internet().password());
-
+        driver.findElement(LoginPassWordTXT).sendKeys(userPassWord);
     }
 
     @When("^the user click on login button$")
     public void theUserClickOnLoginButton() {
-        driver.findElement(LoginBtn).click();
-
+        driver.findElement(LoginBTN).click();
     }
 
     @Then("^the user shall be redirect to my booking page$")
     public void theUserShallBeRedirectToMyBookingPage() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div/div[2]/a")));
-        WebElement MyProfile = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div/div[2]/a"));
-        Assert.assertEquals(true, MyProfile.isDisplayed());
-
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AccountSettingBTN));
+        WebElement MyProfile = driver.findElement(AccountSettingBTN);
+        Assert.assertTrue(MyProfile.isDisplayed());
     }
 
     @Given("^user enter unregistered email$")
     public void userEnterUnregisteredEmail() {
-        driver.findElement(LoginEmailTxt).sendKeys("iamnotregistered@gmail.com");
-
+        driver.findElement(LoginEmailTXT).sendKeys(fakerLogin.internet().emailAddress());
     }
 
-    @Then("^user shall see error message$")
-    public void userShallSeeErrorMessage() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='tooltip error error']//span[@class='tooltiptext']")));
-        WebElement errormessage = driver.findElement(By.xpath("//div[@class='tooltip error error']//span[@class='tooltiptext']"));
-        Assert.assertEquals(errormessage.isDisplayed(), true);
-    }
+    @Then("^user shall see InValid Login Error Message$")
+    public void userShallSeeInValidLogInErrorMessage() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(InvalidLogInMSG));
+        WebElement ErrorMessage = driver.findElement(InvalidLogInMSG);
+        Assert.assertTrue(ErrorMessage.isDisplayed());
+        String InValidLoginMSGText = driver.findElement(InvalidLogInMSG).getText();
+        Assert.assertEquals(InValidLoginMSGText, "!Invalid Email or password");
 
-
-    @And("^user enter the wrong password$")
-    public void userEnterTheWrongPassword() {
-        driver.findElement(LoginPassWordTxt).sendKeys("@test123");
-    }
-
-    @And("^user enter the password less than 8 chars$")
-    public void userEnterThePasswordLessThanChars() {
-        driver.findElement(LoginPassWordTxt).sendKeys("12345");
     }
 
     @Then("^user shall see password error message$")
     public void userShallSeePasswordErrorMessage() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='tooltiptext with-arrow']")));
-        WebElement passworderrormessage = driver.findElement(By.xpath("//span[@class='tooltiptext with-arrow']"));
-        Assert.assertEquals(passworderrormessage.isDisplayed(), true);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(PassWordErrorMSG));
+        WebElement passWordErrorMessage = driver.findElement(PassWordErrorMSG);
+        Assert.assertTrue(passWordErrorMessage.isDisplayed());
+        String PassWordErrorMSGText = driver.findElement(PassWordErrorMSG).getText();
+        Assert.assertEquals(PassWordErrorMSGText, "!Password length must be between 8 to 50 characters");
+    }
+
+    @And("^user enter the wrong password$")
+    public void userEnterTheWrongPassword() {
+        driver.findElement(LoginPassWordTXT).sendKeys("@test123");
+    }
+
+    @And("^user enter the password less than 8 chars$")
+    public void userEnterThePasswordLessThanChars() {
+        driver.findElement(LoginPassWordTXT).sendKeys("12345");
+    }
+
+    @Then("^user shall see empty password error message$")
+    public void userShallSeeEmptyPasswordErrorMessage() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(PassWordErrorMSG));
+        WebElement passWordErrorMessage = driver.findElement(PassWordErrorMSG);
+        Assert.assertTrue(passWordErrorMessage.isDisplayed());
+        String PassWordErrorMSGText = driver.findElement(PassWordErrorMSG).getText();
+        Assert.assertEquals(PassWordErrorMSGText, "!Please enter password");
     }
 
     @And("^user enter the password more than 60 chars$")
-    public void userEnterThePasswordmoreThanChars() {
-        driver.findElement(LoginPassWordTxt).sendKeys("1111111111111111111111111111111111111111111111111111111111111");
+    public void userEnterThePassWordMoreThanChars() {
+        driver.findElement(LoginPassWordTXT).sendKeys("1111111111111111111111111111111111111111111111111111111111111");
     }
-
 
     @And("^user enter an invalid email$")
     public void userEnterAnInValidEmail() {
-        driver.findElement(LoginEmailTxt).sendKeys("M.saYed.89gmailcom");
+        driver.findElement(LoginEmailTXT).sendKeys("M.saYed.89gmailcom");
+    }
 
+    @Then("^user shall see email empty error message$")
+    public void userShallSeeEmptyEmailErrorMessage() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(EmailErrorMSG));
+        WebElement emailErrorMessage = driver.findElement(EmailErrorMSG);
+        Assert.assertTrue(emailErrorMessage.isDisplayed());
+        String EMailErrorMSGText = driver.findElement(EmailErrorMSG).getText();
+        Assert.assertEquals(EMailErrorMSGText, "!Please enter email");
     }
 
     @Then("^user shall see email error message$")
     public void userShallSeeEmailErrorMessage() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='tooltiptext with-arrow']")));
-        WebElement emailerrormessage = driver.findElement(By.xpath("//span[@class='tooltiptext with-arrow']"));
-        Assert.assertEquals(emailerrormessage.isDisplayed(), true);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(EmailErrorMSG));
+        WebElement emailErrorMessage = driver.findElement(EmailErrorMSG);
+        Assert.assertTrue(emailErrorMessage.isDisplayed());
+        String EMailErrorMSGText = driver.findElement(EmailErrorMSG).getText();
+        Assert.assertEquals(EMailErrorMSGText, "!Please enter a valid email");
     }
 
     @And("^user enter an empty email$")
     public void userEnterAnEmptyEmail() {
-        driver.findElement(LoginEmailTxt).sendKeys("");
-
+        driver.findElement(LoginEmailTXT).sendKeys("");
     }
 
     @And("^user enter empty password$")
     public void userEnterEmptyPassword() {
-        driver.findElement(LoginPassWordTxt).sendKeys("");
-
+        driver.findElement(LoginPassWordTXT).sendKeys("");
     }
 
     @And("^user enter an upper case right email$")
     public void userEnterAnUpperCaseRightEmail() {
-        driver.findElement(LoginEmailTxt).sendKeys(userEmail.toUpperCase());
-
+        driver.findElement(LoginEmailTXT).sendKeys(userEmail.toUpperCase());
     }
-
 
     @And("^user logout$")
     public void userLogout() {
-        driver.findElement(ProfileNameBtn).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(LogoutBtn));
-        driver.findElement(LogoutBtn).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginHeaderBtn));
-        String homeText = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h2[1]/span[1]")).getText();
+        driver.findElement(ProfileNameBTN).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LogoutBTN));
+        driver.findElement(LogoutBTN).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginHeaderBTN));
+        String homeText = driver.findElement(HomePageTitle).getText();
         Assert.assertEquals(homeText, "Low Fares");
-
-
     }
 
     @And("^insert new user at database$")
     public void insertNewUserAtDataBase() {
-        DataBase.execute_update(hostName, dbsName, "insert into users (email, \"lastName\",\"firstName\",password,\"storeId\", \"groupId\")values('" + userEmail + "','Sayed','Mahmoud','" + userHashPassWord + "','fly365_com','fly365')");
+        DataBase.execute_update(hostName, dbsName, "insert into users (email, \"lastName\",\"firstName\",password,\"storeId\", \"groupId\",\"isActive\",\"isLocked\")values('" + userEmail + "','Sayed','Mahmoud','" + userHashPassWord + "','fly365_com','fly365',True,False)");
     }
-
 
     @And("^delete new user at database$")
     public void deleteTheNewUserAtDataBase() {
-
         DataBase.execute_query_dbs(hostName, dbsName, "delete from users where email='" + userEmail + "'");
     }
-
 
 }
