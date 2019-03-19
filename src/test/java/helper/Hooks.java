928ee1b8f4a62dc62ed42991494d26960e32f273
Remon Refaat <remon.refaat@fly365.com>
2019-03-19 21:21:06 +0200

    Update in Hooks

--- src/test/java/helper/Hooks.java
+++ src/test/java/helper/Hooks.java
@@ -1,10 +1,27 @@
 package helper;
 
 import cucumber.api.java.After;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import step_definition.HomeTest;
 
 public class Hooks extends TestBase{
+
+    WebDriverWait wait = new WebDriverWait(driver, 20);
+
+    @After
+    public void SignOut(){
+        if (driver.findElement(By.xpath("//div/div[2]/span")).getText().trim().equals("John"))
+        {
+            driver.findElement(By.xpath("//span[@class='el-dropdown-link capitalize text-xs text-white el-dropdown-selfdefine']")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li[contains(text(),'Sign Out')]")));
+            driver.findElement(By.xpath("//li[contains(text(),'Sign Out')]")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='SIGN IN']")));
+        }
+
+    }
 
     @After("@New_Tab")
     public void closeTheNewTab(){
