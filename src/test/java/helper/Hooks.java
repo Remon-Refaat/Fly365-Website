e1957617d327d6e2482fcfd80dfe21967d582335
Remon Refaat <remon.refaat@fly365.com>
2019-04-09 09:10:20 +0200

    Screenshot if the Scenario Fails

--- src/test/java/helper/Hooks.java
+++ src/test/java/helper/Hooks.java
@@ -1,17 +1,33 @@
 package helper;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import step_definition.HomeTest;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Hooks extends TestBase{
 
     WebDriverWait wait = new WebDriverWait(driver, 20);
+
+
+
+    @After
+    public void takeScreenShot(Scenario scenario) throws IOException {
+        if (scenario.isFailed())
+        {
+            File scrFile = ((TakesScreenshot)TestBase.driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "/Screenshots/"+scenario.getName()+".jpg"));
+        }
+    }
 
     @After("@Go_Tab_Again")
     public void goToTheCurrentTab(){
