f4669e39708a964c32e48cfeb23a1ef1fe7bbbee
mohamedhassan <mohamed.emad@fly365.com>
2019-05-09 13:26:00 +0200

    upload headless

--- src/test/java/helper/TestBase.java
+++ src/test/java/helper/TestBase.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -25,14 +27,54 @@ public class TestBase extends AbstractTestNGCucumberTests {
 
     @BeforeSuite
     @Parameters({"browser"})
-    public void startDriver(@Optional("chrome") String browserName) throws MalformedURLException {
+    public void startDriver(@Optional("chrome") String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            Reporter.log("=====Chrome Browser Session Started=====", true);
+            String chromePath = System.getProperty("user.dir") + "/Resources/chromedriver";
+            System.setProperty("webdriver.chrome.driver", chromePath);
 
-        String chromePath = System.getProperty("user.dir") + "/Resources/chromedriver";
-        System.setProperty("webdriver.chrome.driver", chromePath);
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability(ChromeOptions.CAPABILITY, options);
-        driver = new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),options);
+            //to download file in Downloads file
+            String downloadFilepath = System.getProperty("user.dir") + "/Downloads";
+            ChromeOptions options = new ChromeOptions();
+            HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+            chromePrefs.put("profile.default_content_settings.popups", 0);
+            chromePrefs.put("download.default_directory", downloadFilepath);
+            options.setExperimentalOption("prefs", chromePrefs);
+            DesiredCapabilities cap = DesiredCapabilities.chrome();
+            cap.setCapability(ChromeOptions.CAPABILITY, options);
 
+
+            driver = new ChromeDriver(cap);
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            Reporter.log("=====FireFox Browser Session Started=====", true);
+            String firefoxPath = System.getProperty("user.dir") + "/Resources/geckodriver";
+            System.setProperty("webdriver.gecko.driver", firefoxPath);
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("safari")) {
+            Reporter.log("=====Safari Browser Session Started=====", true);
+            driver = new SafariDriver();
+        } else if (browserName.equalsIgnoreCase("headless"))
+        {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setJavascriptEnabled(true);
+            caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                    System.getProperty("user.dir") + "/Resources/phantomjs");
+            String[] phantomJsArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+            caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
+            driver = new PhantomJSDriver(caps);
+
+//            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Resources/chromedriver");
+//            ChromeOptions options = new ChromeOptions();
+//            options.addArguments("--headless");
+//            options.addArguments("--window-size=1920,1080");
+//            driver = new ChromeDriver(options);
+        }
+
+//        String chromePath = System.getProperty("user.dir") + "/Resources/chromedriver";
+//        System.setProperty("webdriver.chrome.driver", chromePath);
+//        ChromeOptions options = new ChromeOptions();
+//        options.setCapability(ChromeOptions.CAPABILITY, options);
+//        driver = new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"),options);
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -40,7 +82,6 @@ public class TestBase extends AbstractTestNGCucumberTests {
         driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
 
         Reporter.log("=====Application Started=====", true);
-
     }
 
     @AfterSuite
@@ -48,6 +89,4 @@ public class TestBase extends AbstractTestNGCucumberTests {
         driver.quit();
         Reporter.log("=====Browser Session End=====", true);
     }
-
-
 }
