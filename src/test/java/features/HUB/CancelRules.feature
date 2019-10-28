d355fac9e588b8387734908e6c4f23062f5b93f7
MohamedEmadHassan <46998396+MohamedEmadHassan@users.noreply.github.com>
2019-10-28 12:50:36 +0200

    Monday (#13)
    * refactor for BVT
    
    * chromedriver and gmail fix
    
    * fix tickets issue
    
    * upload discount
    
    * upload discount
    
    * extra discount scenarios
    
    * another api class
    
    * small update
    
    * last changes
    
    * new changes

--- src/test/java/features/HUB/CancelRules.feature
+++ src/test/java/features/HUB/CancelRules.feature
@@ -1,6 +1,4 @@
 
-@cancelRules
-
 Feature: Create Cancel Rule
 
   Scenario: Verify that agent can create rule & successful message is displayed
@@ -12,7 +10,6 @@ Feature: Create Cancel Rule
     And Submit Rule
     Then Rule Success Message Is Displayed
 
-
   Scenario: Verify that agent can create rule & rule is added to the list
     Given Open hub login page
     And login into hub with super admin
@@ -21,8 +18,6 @@ Feature: Create Cancel Rule
     And Fill Rule Data
     And Submit Rule
     Then Rule Is Added To The List
-
-
 
   Scenario: Verify that agent can update rule and successful message is displayed
     Given Open hub login page
@@ -33,7 +28,6 @@ Feature: Create Cancel Rule
     And Submit Rule
     Then Rule Updated Message Is Displayed
 
-
   Scenario: Verify that agent can update rule and rule is updated in the list
     Given Open hub login page
     And login into hub with super admin
@@ -42,7 +36,6 @@ Feature: Create Cancel Rule
     And Edit Rule Name
     And Submit Rule
     Then Rule Is Updated In The List
-
 
   Scenario: Verify That Agent Can Disable Rule And Successful Message Is Displayed
     Given Open hub login page
@@ -53,7 +46,6 @@ Feature: Create Cancel Rule
     And Submit Rule
     Then Rule Updated Message Is Displayed
 
-
   Scenario: Verify That Agent Can Disable Rule Status Is Disabled In The List
     Given Open hub login page
     And login into hub with super admin
@@ -62,7 +54,6 @@ Feature: Create Cancel Rule
     And Change Status To Disabled
     And Submit Rule
     Then Rule status Is Updated In The List
-
 
    Scenario: Verify That Agent Can't Create Rule With the Same Name
      Given Open hub login page
--- src/test/java/step_definition/Discount.java
+++ src/test/java/step_definition/Discount.java
@@ -1,6 +1,9 @@
 package step_definition;
 
 import com.github.javafaker.Faker;
+import com.google.gson.JsonObject;
+import com.mongodb.util.JSON;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -10,6 +13,7 @@ import helper.GeneralMethods;
 import helper.TestBase;
 import org.apache.commons.lang3.ObjectUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -17,17 +21,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.lang.reflect.Array;
 import java.security.Key;
-import java.util.Locale;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Discount extends TestBase {
 
     GeneralMethods gm = new GeneralMethods();
     APIUtility api = new APIUtility();
-    WebDriverWait wait = new WebDriverWait(driver, 30);
+    FluentWait<WebDriver> wait = new WebDriverWait(driver, 30, 1000).withMessage("Element not found yet");
 
     private By systemBarMenuBTN = By.xpath("//div[@class='el-submenu__title']//img");
     private By discountBTN = By.xpath("/html/body/div[2]/ul/li[2]/a/span[2]");
@@ -72,9 +79,9 @@ public class Discount extends TestBase {
     @And("^Fill required data for discount rule$")
     public void fillRequiredDataForDiscountRule() throws InterruptedException {
         wait.until(ExpectedConditions.visibilityOfElementLocated(discountNameTXT));
-        driver.findElement(discountNameTXT).sendKeys("Test Discount");
+        driver.findElement(discountNameTXT).sendKeys(discountEnteredName);
         //gm.selectFromDDLHub("storeId", "fly365_au");
-        driver.findElement(discountStoreDDL).sendKeys("fly365_com");
+        driver.findElement(discountStoreDDL).sendKeys("fly365_nz");
         driver.findElement(discountStoreDDL).sendKeys(Keys.ENTER);
         driver.findElement(discountPercentTXT).sendKeys("5");
         driver.findElement(excludeCountryTXT).sendKeys("No");
@@ -97,7 +104,8 @@ public class Discount extends TestBase {
 
     @And("^Make Search from API$")
     public void makeSearchFromAPI() throws InterruptedException {
-            allTrips = api.sendPostRequest("https://nz.fly365stage.com/api/flight-search/search", api.oneWayAPI());
+        Thread.sleep(5000);
+        allTrips = api.sendPostRequest("https://nz.fly365stage.com/api/flight-search/search", api.oneWayAPI());
     }
 
     @And("^Open certain store$")
@@ -114,10 +122,14 @@ public class Discount extends TestBase {
     }
 
     @And("^Update Name of discount rule$")
-    public void updateNameOfDiscountRule() {
+    public void updateNameOfDiscountRule() throws InterruptedException {
         wait.until(ExpectedConditions.visibilityOfElementLocated(discountNameTXT));
-        driver.findElement(discountNameTXT).sendKeys(Keys.chord(Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE,Keys.BACK_SPACE));
-        driver.findElement(discountNameTXT).sendKeys("Update Test Discount");
+        Thread.sleep(1000);
+        int nameLength = driver.findElement(discountNameTXT).getAttribute("value").length();
+        for (int i = 0 ; i < nameLength ; i++){
+            driver.findElement(discountNameTXT).sendKeys(Keys.chord(Keys.BACK_SPACE));
+        }
+        driver.findElement(discountNameTXT).sendKeys(discountEnteredName);
 
     }
 
@@ -127,7 +139,7 @@ public class Discount extends TestBase {
         Thread.sleep(3000);
         driver.findElement(clearSelectionBTN).click();
         Thread.sleep(3000);
-        driver.findElement(discountStatusTXT).sendKeys("Disabled");
+        driver.findElement(discountStatusTXT).sendKeys("disabled");
         driver.findElement(discountStatusTXT).sendKeys(Keys.ENTER);
     }
 
@@ -152,9 +164,8 @@ public class Discount extends TestBase {
 
     @Then("^Check Discount rule applied$")
     public void checkDiscountRuleApplied() throws InterruptedException {
-        Thread.sleep(8000);
-        System.out.println(discountEnteredName);
-        discountName = api.getDiscountName(allTrips,1);
+        Thread.sleep(3000);
+        discountName = api.getDiscountName(allTrips, 1);
         System.out.println(discountName);
         Assert.assertEquals(discountName, discountEnteredName);
     }
@@ -168,16 +179,49 @@ public class Discount extends TestBase {
 
 
     @Then("^Check Discount rule disabled$")
-    public void checkDiscountRuleDisabled() {
+    public void checkDiscountRuleDisabled() throws InterruptedException {
+        Thread.sleep(20000);
         discountStatus = api.getDiscountStatus(allTrips, 1);
-        Assert.assertTrue(false, discountStatus);
+        Assert.assertEquals(discountStatus, "null");
     }
 
     @And("^Delete new discount from database$")
     public void deleteNewDiscountFromDatabase() {
-            DataBase.execute_query_dbs(hostName, dbsName, "select from campaigns where \"name\" = '" + discountEnteredName +"'");
-            if (DataBase.data != null){
-                DataBase.execute_query_dbs(hostName, dbsName, "delete from campaigns where \"name\" = '" + discountEnteredName +"'");
-            }
+        DataBase.execute_query_dbs(hostName, dbsName, "delete from campaigns where \"name\" = '" + discountEnteredName + "'");
         }
+
+    @And("^Check discount from database to be applied$")
+    public void CheckDiscountFromDatabaseToBeApplied() {
+            DataBase.execute_query_dbs(hostName, dbsName, "Update settings \n" +
+                    "SET \"isActive\" = 'TRUE'\n" +
+                    "WHERE id='715dbed1-93bb-4430-915e-f862596ceaff'");
+    }
+
+    @And("^Make Browser back$")
+    public void makeBrowserBack() throws InterruptedException {
+        gm.makeBrowserBack();
+    }
+
+    @And("^Book a trip from API on \"(.*)\" and get \"(.*)\"$")
+    public String bookATripFromAPIOnAndGet(String domain, String responseField) throws Throwable {
+        String requestUrl = "https://api.fly365" + domain + ".com/flight-search/search";
+        String allAvailableTrips = api.sendPostRequest(requestUrl, api.oneWayAPI());
+        String itinaryID = api.getItineraryId(allAvailableTrips, 2);
+        String cardID = api.createCart(itinaryID, domain);
+        api.addPassenger(cardID, domain);
+        String[] checkoutResponse = api.checkoutTrip(cardID, domain);
+        String finalResponse = api.getresult(checkoutResponse[1], checkoutResponse[0]);
+        JSONObject jObject = new JSONObject(finalResponse);
+        JSONArray arr = jObject.getJSONArray("products");
+        System.out.println(arr);
+
+        String discountNAM = null;
+        for (int i = 0; i < arr.length(); i++) {
+            JSONArray array_1 = arr.getJSONObject(i).getJSONArray("options");
+            JSONObject OBJ1 = array_1.getJSONObject(0);
+            discountNAM = OBJ1.getJSONObject("discounts").get("name").toString();
+        }
+        System.out.println(discountNAM);
+        return responseField;
+    }
 }
